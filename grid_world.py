48d6e2f335e81fd881d47f9152204941a7eba77f
EllipseHEP <cyrille.mathieu-ton@hepl.ch>
2025-04-07 09:33:49 +0200

    Create grid_world.py

---
+++ grid_world.py
@@ -0,0 +1,23 @@
+# run_gymnasium_env.py
+
+import gymnasium
+import gymnasium_env
+import time
+
+env = gymnasium.make('gymnasium_env:gymnasium_env/GridWorld', render_mode="human")
+#env = gymnasium.make('gymnasium_env:gymnasium_env/GridWorld', render_mode="rgb_array")
+
+obs = env.reset()
+
+obs, info = env.reset()
+
+for _ in range(50):  # Run for 50 steps
+    action = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(action)
+    
+    if terminated:
+        obs, info = env.reset()
+    
+    time.sleep(0.5)  # Pause so you can see the moves
+
+env.close()
